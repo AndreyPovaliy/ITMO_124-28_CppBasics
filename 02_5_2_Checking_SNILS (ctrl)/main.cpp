3c2b09c2164a5f7f01da9ba5702da68abb68207f
Andrey Povaliy <116582306+AndreyPovaliy@users.noreply.github.com>
2023-06-07 14:10:01 +0300

    New

--- 02_5_2_Checking_SNILS (ctrl)/main.cpp
+++ 02_5_2_Checking_SNILS (ctrl)/main.cpp
@@ -1,50 +1,90 @@
 #include <iostream>
 #include <windows.h>
+using std::cin;
+using std::cout;
+using std::size;
+
 using namespace std;
 
-int main() {
+int main()
+{
     system("chcp 1251");
+    // объявление используемых переменных и массива
+    int i,j;       
+    int const n = 9;
+    int snils[n];
+    int sum = 0; 
+    int nSum = 0;
+    int ctrlNumber;
+    int ctrlNumberCalcul;
 
-    
+    // логической переменной для проверки
+    bool check = false;
 
-    int number_snils;
-    int sumOfNumberSnils = 0;
-    int inputCheckNum, checkNum;
-    int limitation = 0;
-
-    cout << "Введите СНИЛС: ";
-    for (int i = 1; i <= 9; i++) {
-        cin >> number_snils;
-
-        if (i != 0) {
-            sumOfNumberSnils += number_snils * i;
-        }
-        else {
-            inputCheckNum = number_snils;
-        }
+    for (i = 0; i < n; i++)
+    {
+        cout << "Введите "<< i+1 << " номер СНИЛС" << endl;
+        cin >> snils[i]; 
     }
-    cout << sumOfNumberSnils << endl;
 
-    /*limitation = sum % 101;
+    cout << "Введите контрольное число" << endl;
+    cin >> ctrlNumber;
 
+    // сумма произведений введенных значений
+    for (i = 0, j =n ; i < n; i++, j--)
+    {
+        sum += snils[i] * j;
+    }
+   
+    // вычисление расчетного контрольго числа
     if (sum < 100) {
-        checkNum = inputCheckNum;
+        ctrlNumberCalcul = sum;
     }
-    else if (sum == 100 || sum == 101) {
-        checkNum = 0;
+    else
+        if (sum == 100 || sum == 101) {
+            ctrlNumberCalcul = 00;
+
+        }
+        else
+            nSum = sum % 101;
+
+    if (nSum < 100) {
+        ctrlNumberCalcul = nSum;
     }
-    else if (remainder < 100) {
-        checkNum = limitation;
-    }
-    else {
-        checkNum = 0;
+    else
+        if (nSum == 100 || nSum == 101) {
+            ctrlNumberCalcul = 00;
+        }
+        
+    cout <<"Контрольное число должно быть:" << ctrlNumberCalcul << endl;
+
+
+    // проверка введенных значений
+    for (i = 0; i < n; i++) {
+        if (snils[i] == snils[i + 1]) {
+            check = false;
+            break;
+        }
+        else 
+            if (snils[0] / 10 >=1) {
+                check = false;
+                break;
+            }
+            else
+                if (ctrlNumber != ctrlNumberCalcul) {
+                    check = false;
+                    break;
+                }
+            else check = true;
+
     }
 
-    cout << "Результат проверки: ";
-    if (checkNum == inputCheckNum) {
-        cout << true << std::endl;
+
+    // вывод валидности СНИЛС
+    if (check == true) {
+        cout << "Валидный снилс" << endl;
     }
-    else {
-        cout << false << std::endl;
-    }*/
+    else cout << "Невалидный снилс" << endl;
+
+    return 0;
 }
